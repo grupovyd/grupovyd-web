e2caebd971e7779fe0b2d8817ddf21c3452ccb75
Grupo V&D <administracion@grupovyd.com.pe>
2026-08-05 15:40:26 -0500

    Mejora SEO inicial: favicon, Open Graph, Twitter Cards, robots.txt, sitemap y preview social

---
+++ js/components.js
@@ -0,0 +1,27 @@
+async function cargarComponente(id, archivo){
+
+    const contenedor = document.getElementById(id);
+
+    if(!contenedor) return;
+
+    const respuesta = await fetch(archivo);
+
+    contenedor.innerHTML = await respuesta.text();
+
+}
+
+async function iniciarComponentes(){
+
+    await cargarComponente(
+        "navbar-container",
+        "/componentes/navbar.html"
+    );
+
+    await cargarComponente(
+        "footer-container",
+        "/componentes/footer.html"
+    );
+
+}
+
+iniciarComponentes();
